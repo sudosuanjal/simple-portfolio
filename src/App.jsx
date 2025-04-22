68d7e9c00e79f754810389052834c720f1e3b481
sudosuanjal <anjalehackchey101@gmail.com>
2025-04-22 12:53:17 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>simple-portfolio for sudosuanjal</h1>
+    </>
+  );
+}
+
+export default App;
